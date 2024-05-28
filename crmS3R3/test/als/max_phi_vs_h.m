b6525602be637b6a3a110885ab61fd2c206ef954
Denise Tumiotto <denise.tumiotto@gmail.com>
2024-05-28 15:01:02 +0200

    plots for cosserat rod drift off

--- crmS3R3/test/als/max_phi_vs_h.m
+++ crmS3R3/test/als/max_phi_vs_h.m
@@ -1,20 +1,10 @@
-function max_phi_vs_h(sols, feature, side)
+function max_phi_vs_h(sols,name)
 
-    % 'feature' : space, a_baumgarte
-    % 'side' : 1 = TRUE, 0 = FALSE
+    % name: C
+    % name: a_baumgarte
 
-    if nargin < 1
-        error('Not enough arguments!')
-    elseif nargin < 2
-        % default value
-        feature = 'a_baumgarte';
-        side = 0;
-    elseif nargin < 3
-        side = 0;
-    end
-
-    if isempty(feature)
-        feature = 'a_baumgarte';
+    if nargin < 2
+        name = 'a_baumgarte';
     end
 
     size_s = max(size(sols));
@@ -24,91 +14,45 @@ function max_phi_vs_h(sols, feature, side)
     end
 
     colors = linspecer(5);
-    spaces = [];
-    parameters = [];
+    parameter = [];
+    tol = 1e-4;
+
+    figure()
+    hold on
 
     for i = 1:size_s
-        if strcmp(feature,'a_baumgarte')
-            if side
-                if ~exist('n_fig','var')
-                    figure()
-                    n_fig = get(gcf,'Number');
-                else
-                    figure(n_fig)
-                end
-                k = find(spaces==sols{i}.problemset);
-                if isempty(k)
-                    spaces = [spaces sols{i}.problemset];
-                    subplot(1,2,length(spaces))
-                    plt{length(spaces)} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), '-', ...
-                        'DisplayName', num2str(sols{i}.problemset), 'LineWidth', 1.5);
-                else
-                    subplot(1,2,k)
-                    plt{k}.XData = [plt{k}.XData sols{i}.h];
-                    plt{k}.YData = [plt{k}.YData max(vecnorm(sols{i}.rslt.Phi))];
-                end
-            else
-                j = find(spaces==sols{i}.problemset);
-                if isempty(j)
-                    spaces = [spaces sols{i}.problemset];
-                    figure(length(spaces))
-                    hold on
-                    k = find(parameters==sols{i}.a_baumgarte);
-                    if isempty(k)
-                        parameters = [parameters sols{i}.a_baumgarte];
-                        plt{length(spaces),length(parameters)} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), 'o-', ...
-                            'DisplayName', ['$\gamma$ = ' num2str(sols{i}.a_baumgarte)], 'LineWidth', 1.5);
-                    else
-                        plt{length(spaces),k} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), 'o-', ...
-                            'DisplayName', ['$\gamma$ = ' num2str(sols{i}.a_baumgarte)], 'LineWidth', 1.5);
-                    end
-                else
-                    figure(j)
-                    k = find(parameters==sols{i}.a_baumgarte);
-                    if isempty(k)
-                        parameters = [parameters sols{i}.a_baumgarte];
-                        plt{j,length(parameters)} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), 'o-', ...
-                            'DisplayName', ['$\gamma$ = ' num2str(sols{i}.a_baumgarte)], 'LineWidth', 1.5);
-                    else
-                        if isempty(plt{j,k})
-                            plt{j,k} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), 'o-', ...
-                                'DisplayName', ['$\gamma$ = ' num2str(sols{i}.a_baumgarte)], 'LineWidth', 1.5);
-                        else
-                            plt{j,k}.XData = [plt{j,k}.XData sols{i}.h];
-                            plt{j,k}.YData = [plt{j,k}.YData max(vecnorm(sols{i}.rslt.Phi))];
-                        end
-                    end
-                end
-            end
-        elseif strcmp(feature,'space')
-            disp('later')
-        else
-            disp('not implemented')
-        end
-        ax=gca;
-        ax.FontSize = 14;
-        ax.PlotBoxAspectRatio = [1 1 1];
-        set(gca, 'YScale', 'log', 'XScale', 'log')
-        switch sols{i}.liegroup
-            case 1 % SO(3)
-                disp('function not implemented for SO(3)')
-            case 2 % S³
-                disp('function not implemented for S^3')
-            case 3 % SO(3) x R³
-                space = '$SO(3)\times R^3$';
-            case 4 % S³ x R³    
-                space = '$S^3\times R^3$';
-            case 5 % SE(3)
-                space = '$SE(3)$';
-            case 6 % S³ |x R³
-                space = '$S^3\ \vert\times R^3$';
-            case 7 % UDQ
-        end
-        title(['Max constraint residual in ', space],'FontSize',14,'Interpreter','latex')
-        xlabel('Time step size (h)','FontSize',14,'Interpreter','latex')
-        ylabel('$\max(\Vert\Phi(R,x)\Vert_2)$','FontSize',14,'Interpreter','latex')
-        legend('Location','best','AutoUpdate','on','Interpreter','latex')
-        grid on
 
+        if strcmp(name, 'a_baumgarte')
+            the_par = sols{i}.a_baumgarte;
+            k = find(parameter==the_par);
+            text = ['$\gamma =$' num2str(sols{i}.(name))];
+        elseif strcmp(name, 'C')
+            the_par = sols{i}.a_baumgarte*sols{i}.h;
+            k = find(parameter<the_par+tol & parameter>the_par-tol);
+            text = ['$C=$' num2str(sols{i}.a_baumgarte*sols{i}.h)];
+        else
+            text = '';
+            k = [];
+        end
+
+        if isempty(k)
+            parameter = [parameter the_par];
+            plt{length(parameter)} = loglog(sols{i}.h, max(vecnorm(sols{i}.rslt.Phi)), 'o-', ...
+                'DisplayName', text, 'LineWidth', 1.5, 'Color', colors(mod(i,k)+1,:));
+        else
+            plt{k}.XData = [plt{k}.XData sols{i}.h];
+            plt{k}.YData = [plt{k}.YData max(vecnorm(sols{i}.rslt.Phi))];
+        end
     end
+
+    ax=gca;
+    ax.FontSize = 14;
+    ax.PlotBoxAspectRatio = [1 1 1];
+    set(gca, 'YScale', 'log', 'XScale', 'log')
+
+    title(['{\bf{Max constraint residual vs time step size}} $h$'],'FontSize',14,'Interpreter','latex')
+    xlabel('Time step size ($h$)','FontSize',14,'Interpreter','latex')
+    ylabel('$\max(\Vert{\bf{\Phi}}(q)\Vert_2)$','FontSize',14,'Interpreter','latex')
+    legend('Location','best','AutoUpdate','on','Interpreter','latex')
+    grid on
 end
--- crmS3R3/test/als/phi_residual_plot.m
+++ crmS3R3/test/als/phi_residual_plot.m
@@ -1,23 +1,46 @@
-function phi_residual_plot(sols)
+function phi_residual_plot(sols,name)
+
+    % name: C
+    % name: h
+    % name: a_baumgarte
+
+    if nargin < 2
+        name = 'a_baumgarte';
+    end
 
     size_s = max(size(sols));
     colors = linspecer(min(4, size_s + 1)+1);
-    style  = ['-',':','--','-.'];
+    style  = {'-', ':', '--', '-.'};
 
+    if ~isfield(sols{1},'h')
+        sols = create_h(sols);
+    end
+    
     figure()
     hold on
     for i = 1:size_s
+        if strcmp(name, 'a_baumgarte')
+            text = ['$\gamma =$' num2str(sols{i}.(name))];
+        elseif strcmp(name, 'C')
+            text = ['$C=$' num2str(sols{i}.a_baumgarte*sols{i}.h(1))];
+        elseif strcmp(name, 'h')
+            text = ['$h=$' num2str(sols{i}.h)];
+        else
+            text = '';
+        end
+
         plot(sols{i}.rslt.t, vecnorm(sols{i}.rslt.Phi), ...
-            'Color', colors(mod(i,size(colors,1))+1,:), 'LineWidth', 1.5, ...
-            'LineStyle', style(mod(i,4)+1))
+            'DisplayName', text, 'Color', colors(mod(i,size(colors,1))+1,:), ...
+            'LineWidth', 1.5, 'LineStyle', style{mod(i,4)+1})
     end
     ax=gca;
     ax.FontSize = 14;
     ax.PlotBoxAspectRatio = [1 1 1];
     ax.YScale = 'log';
-    title(['Constraint residual vs time'],'FontSize',14,'Interpreter','latex')
+    title('Constraint residual vs time','FontSize',14,'Interpreter','latex')
     xlabel('Time $(t)$','FontSize',14,'Interpreter','latex')
     ylabel('$\Vert\Phi(R,x)\Vert_2$','FontSize',14,'Interpreter','latex')
+    legend('Interpreter','latex')
     grid on
     box on
 end
