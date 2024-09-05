0ad749f472915affe5266f58848a2c89f7b4e410
Denise Tumiotto <denise.tumiotto@gmail.com>
2024-09-05 12:33:51 +0200

    added comments on read me + new plots

--- crmS3R3/test/als/cpu_vs_error_plot.m
+++ crmS3R3/test/als/cpu_vs_error_plot.m
@@ -1,27 +1,55 @@
-function cpu_vs_error_plot(sols, my_var)
+function cpu_vs_error_plot(sols, varargin)
+    % The first element of varargin should always be the variable of which we evaluate the global error!
+    % If it is empty is assumed to be just q
 
-    if nargin < 2
+    if nargin < 1
+        error('Not enough arguments!')
+    end
+    
+    size_var = max(size(varargin));
+    if size_var > 0 && ~isempty(varargin{1})
+        my_var = varargin{1};
+    else
         my_var = 'q';
     end
 
     size_s = max(size(sols));
-    colors = linspecer(min(4, size_s + 1)+1);
+    colors = linspecer(size_s + 1);
 
     figure()
     hold on
 
     for i = 1:size_s
         if sols{i}.err.abs.(my_var) > 0
-            if sols{i}.stab2 == 0
-                text = 'Original Solution';
-            elseif sols{i}.stab_proj == 0
-                text = ['Baumgarte stabilization, $\gamma=$' num2str(sols{i}.a_baumgarte)];
-            else
-                text = ['Projection, $i_{\max}=$' num2str(sols{i}.imax)];
+            text = [];
+            for j = 1:size_var-1
+                switch lower(varargin{j+1})
+                case {'stab'}                    
+                    if sols{i}.stab2 == 0
+                        text = [text 'Original Solution'];
+                    elseif sols{i}.stab_proj == 0
+                        text = [text 'Baumgarte stabilization, $\gamma=$' num2str(sols{i}.a_baumgarte)];
+                    else
+                        text = [text 'Projection, $i_{\max}=$' num2str(sols{i}.imax)];
+                    end
+                case {'n'}
+                    text = [text 'N=' num2str(sols{i}.n)];
+                case {'step_control'}
+                    if sols{i}.step_control == 1
+                        text = [text 'variable time step size'];
+                    else
+                        text = [text 'fixed time step size'];
+                    end
+                otherwise
+                    warning(['' varargin{j-1} ''' not yet implemented']);
+                end
+                if j < size_var-1
+                    text = [text ', '];
+                end                   
             end
             plot(sols{i}.err.rel.(my_var),sols{i}.stats.cpu_time,'o',...
-                'MarkerSize', 5, 'MarkerEdgeColor', colors(mod(i,size(colors,1))+1,:),...
-                'MarkerFaceColor', colors(mod(i,size(colors,1))+1,:), 'DisplayName',text);
+                'MarkerSize', 5, 'MarkerEdgeColor', colors(i,:),...
+                'MarkerFaceColor', colors(i,:), 'DisplayName',text);
         end
     end
 
